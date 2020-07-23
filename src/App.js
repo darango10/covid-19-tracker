6373fa6a63491f642cdf05106661338c1d5fee8d
Daniel <arangomonsalve.daniel@gmail.com>
2020-07-23 18:47:30 -0500

    Table, Map

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import InfoBox from "./InfoBox";
 import Map from "./Map";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import Table from "./Table";
+import {sortData} from "./util";
+import LineGraph from "./LineGraph";
+import 'leaflet/dist/leaflet.css'
+
 
 function App() {
 
@@ -14,6 +19,20 @@ function App() {
 
     const [countries, setCountries,] = useState([]);
     const [country, setCountry] = useState(['worldwide']);
+    const [countryInfo, setCountryInfo] = useState({});
+    const [tableData, setTableData] = useState([]);
+    const [mapCenter, setMapCenter] = useState({lat: 34.80746, lng: -40.4796});
+    const [mapZoom, setMapZoom] = useState(3);
+    const [mapCountries, setMapCountries] = useState([]);
+
+
+    useEffect(() => {
+        fetch('https://disease.sh/v3/covid-19/all')
+            .then(res => res.json())
+            .then(data => {
+                setCountryInfo(data)
+            })
+    },[])
 
     useEffect(() => {
         const getCountriesData = async () => {
@@ -24,7 +43,9 @@ function App() {
                         name: country.country,
                         value: country.countryInfo.iso2
                     }))
-
+                    const sortedData = sortData(data)
+                    setTableData(sortedData);
+                    setMapCountries(data)
                     setCountries(countries);
                 })
         }
@@ -32,10 +53,25 @@ function App() {
         getCountriesData()
     }, [])
 
-    const onCountryChange = (e) => {
+    const onCountryChange = async (e) => {
         const countryCode = e.target.value
-        setCountry(countryCode)
+
+
+        const url = countryCode === 'worldwide' ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+
+        await fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data)
+                setCountry(countryCode);
+                setCountryInfo(data)
+                setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+                setMapZoom(5)
+            })
+
     }
+
+    // console.log(countryInfo)
 
     return (
         <div className="App">
@@ -54,19 +90,22 @@ function App() {
                 </div>
 
                 <div className="app_stats">
-                    <InfoBox title={'Coronavirus cases'} cases={123} total={1.2}/>
-                    <InfoBox title={'Recovered'} cases={1234} total={3000}/>
-                    <InfoBox title={'Deaths'} cases={1} total={4000}/>
+                    <InfoBox title={'Coronavirus cases'} cases={countryInfo.todayCases} total={countryInfo.cases}/>
+                    <InfoBox title={'Recovered'} cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
+                    <InfoBox title={'Deaths'} cases={countryInfo.todayDeaths} total={countryInfo.deaths}/>
                 </div>
                 {/*Map*/}
-                <Map/>
+                <Map center={mapCenter} zoom={mapZoom} countries={mapCountries}/>
             </div>
             <Card className="app_right">
                 <CardContent>
                     <h3>Live Cases by Country</h3>
+                    <Table countries={tableData}/>
                     <h3>Worldwide new cases</h3>
+                    <LineGraph/>
                 </CardContent>
                 {/*Table*/}
+
                 {/*Graph*/}
 
             </Card>
--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {Map as LeafletMap, TileLayer} from 'react-leaflet'
+import './Map.css'
+import {showDataOnMap} from "./util";
 
-const Map = () => {
+const Map = ({center, zoom, countries, casesType}) => {
     return (
         <div className='map'>
-            <h1>I'm a map</h1>
+            <LeafletMap center={center} zoom={zoom}>
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {showDataOnMap(countries, casesType)}
+            </LeafletMap>
         </div>
     );
 };
